aa57710b0f3de019adf4fe74caa92d90bdfaf3f3
Aishwar Pathak <shubhum19@gmail.com>
2024-11-21 13:47:37 +0530

    boilerplate added

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+        VerifyCsrfToken::except([
+            '/reserve-seats',  // Exclude the POST /api/reserve-seats route
+        ]);
+    }
+}
